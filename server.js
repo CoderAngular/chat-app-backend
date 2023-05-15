012fd87cfdfc47d974bee321228536f7d8091fde
CoderAngular <abhik.cloud@gmail.com>
2023-05-16 00:09:26 +0530

    chat app final commit

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const Express = require('express');
 const mongoose = require('mongoose');
 const Path = require('path');
 const app = Express();
-//const socket = require('socket.io');
+const socket = require('socket.io');
+const http = require('http');
+const cors = require('cors');
+const moment = require('moment/moment');
 
+var users = [];
+var sessions = [];
 
 app.use(Express.json());
-//const server = Http.createServer(app);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//Set static folder
+app.use(Express.static(Path.join(__dirname,'dist/chat-app')));
 
 app.post('/userSignup', (req, res) => {
     const user = new User({
@@ -27,12 +31,12 @@ app.post('/userSignup', (req, res) => {
                 });
             }
             else{
-                saveinDB(user).then(
+                saveUserinDB(user).then(
                     function(value){
                         console.log("success=", value);
                         res.json({
                             statusCode: 200,
-                            statusMessage: "User created successfully!"
+                            statusMessage: "User created successfully. Please login to continue."
                         });
                     },
                     function(error){
@@ -69,7 +73,8 @@ app.post('/userLogin', (req, res) => {
                 if(value.password == req.body.loginPassword){
                     res.json({
                         statusCode: 200,
-                        statusMessage: "User logged in successfully!"
+                        statusMessage: "User logged in successfully.",
+                        user: value
                     });
                 }
                 else{
@@ -96,13 +101,30 @@ app.post('/userLogin', (req, res) => {
     );
 });
 
-async function saveinDB(user){
+//Save the user in DB
+async function saveUserinDB(user){
     try{
         const result = await user.save();
         console.log(result);
     }
     catch(ex){
         console.log(ex.message);
+    }
+}
+
+//Save session in DB on disconnect
+async function saveSessioninDB(session){
+    try{
+        const newSession = new Session({
+            sessionId: session.sessionId,
+            messages: session.messages
+        });
+        const result = await newSession.save();
+        console.log("Session saved");
+        console.log(result);
+    }
+    catch(ex){
+        console.log("error while saving session =>"+ex.message);
     }
 }
 
@@ -127,54 +149,106 @@ const userSchema = new mongoose.Schema({
 //create user model
 const User = mongoose.model('User', userSchema);
 
-//============================example to return the post response to client=================================
-// app.post('/signup', async (req, res) => {
+//Create session schema
+const sessionSchema = new mongoose.Schema({
+    sessionId: {type: String, required: true},
+    messages: {type: Array, required: true}
+});
 
-// const var1 = await User.find({email: req.body.email})
-//     .then(res => {
-//         if (res.length === 0) {
-//             console.log('didnt find the email')
-
-//             const id = uuidv4()
-//             const code = '1234'
-
-//             //I want to send this object as a response to the post request
-//             const obj = {id, code}
-
-
-//             Model.create(obj)
-
-//             return obj
-
-//         } else {
-//             console.log('Error')
-//         }
-//     })
-// return res.json(var1)
-// }
-
-//====================================================================
+//create session model
+const Session = mongoose.model('Session', sessionSchema);
 
 //===================SOCKET IO CODES========================
-//const io = socket(server);
 
-//middleware to authenticate
-// io.use( (socket, next) => {
-//     if(socket.handshake.headers.username === 'bbb'){
-//         next();
-//     }
-// });
+const server = http.createServer(app);
+
+const io = socket(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+  });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 //Run when client connects
-// io.on('connection', socket => {
-//     console.log('New Connection...');
-//     console.log(socket.server.engine.clientsCount);
-// })
+io.on('connection', socket => {
+    console.log('New ws Connection...');
+    var newSession = {
+        sessionId: socket.id,
+        messages: []
+    }
+    sessions.push(newSession);
 
+    socket.on('joinChat', (username) => {
+        //Welcome message to new user
+        socket.emit('message', formatMessage('Chatbot','Welcome to chat group!'));
 
+        //Add user
+        addUser(socket.id, username);
 
+        //Notify everyone when a new user joins except current user
+        socket.broadcast.emit('message', formatMessage('Chatbot',`${username} has joined!`));
+        
+        //Broadcast users list to every users
+        io.emit('users', users);
+    })
 
+    //Listen to chat messages
+    socket.on('chatMessage', (msg) => {
+        //broadcast every message to everyone
+        var message = formatMessage(`${msg.user}`, msg.message);
+        io.emit('message', message);
+        sessions.map( s => {
+            s.messages.push(message);
+        });
+    })
 
-// Mongoose.connect('mongodb://localhost/chat-app')
-//     .then( () => console.log('Connected to mongodb...'))
-//     .catch( () => console.log('Could not connect to mongodb...', err));
+    //Notify everyone when a user disconnects
+    socket.on('disconnect', () => {
+        const removedUser = removeUser(socket.id);
+        const removedSession = removeSession(socket.id);
+        console.log(removedSession);
+        saveSessioninDB(removedSession);
+        console.log(sessions);
+        io.emit('message', formatMessage('Chatbot',`${removedUser.username} has left!`));
+
+        //Broadcast users list to every users
+        io.emit('users', users);
+    })
+})
+
+function formatMessage(username,message){
+    return {
+        username,
+        message,
+        time: moment().format('h.mm a')
+    }
+}
+
+function addUser(sid, username){
+    let newUser = {
+        sid,
+        username
+    }
+    users.push(newUser);
+}
+
+function removeUser(sid){
+    const index = users.findIndex(user => user.sid === sid);
+
+    if(index != -1){
+        return users.splice(index,1)[0];
+    }
+}
+
+function removeSession(id){
+    const index = sessions.findIndex(session => session.sessionId === id);
+    console.log(index);
+
+    if(index != -1){
+        return sessions.splice(index,1)[0];
+    }
+}
